f3a383fde5896cbfd9da6a0e8812281f2519a69b
Aayam <aayam2071@gmail.com>
2021-10-20 19:46:54 -0400

    implemented Postgres database and added sample records and queries

--- sql/KnightBites.sql
+++ sql/KnightBites.sql
@@ -1,22 +1,62 @@
-DROP TABLE IF EXISTS PlayerGame;
-DROP TABLE IF EXISTS Game;
-DROP TABLE IF EXISTS Player;
+--Drop previous versions of the table if they exist, in reverse order of foreign keys
+DROP TABLE IF EXISTS Post CASCADE;
+DROP TABLE IF EXISTS Student CASCADE;
+DROP TABLE IF EXISTS Restaurant CASCADE;
 
--- Create the schema.
-CREATE TABLE User (
-	email varchar(50) PRIMARY KEY NOT NULL,
+-- Create the schema
+CREATE TABLE Student (
+	email varchar(30) PRIMARY KEY NOT NULL,
 	firstName varchar(50) NOT NULL,
-	lastName varchar(50)
+	lastName varchar(50),
+	bio varchar(150)
 	);
 
-CREATE TABLE Player (
-	ID SERIAL PRIMARY KEY,
-	email varchar(50) NOT NULL,
-	name varchar(50)
+CREATE TABLE Restaurant (
+	name varchar(50),
+	address varchar (60) NOT NULL,
+	openingTime time,
+	closingTime time,
+	priceRange integer, -- Three tiers of prices ranging from 1 through 3
+	discount float,
+	PRIMARY KEY(name, address)
 	);
 
-CREATE TABLE PlayerGame (
-	gameID integer REFERENCES Game(ID), 
-	playerID integer REFERENCES Player(ID),
-	score integer
+CREATE TABLE Post (
+	studentEmail varchar(30) REFERENCES Student(email),
+	restaurantName varchar(50),
+	restaurantAddress varchar(60),
+	FOREIGN KEY (restaurantName , restaurantAddress) REFERENCES Restaurant(name, address),
+	postTime timestamp,
+	meetupTime timestamp,
+	PRIMARY KEY(StudentEmail, postTime)
 	);
+
+-- Allow Students to select data from the tables
+GRANT SELECT ON Student TO PUBLIC;
+GRANT SELECT ON Restaurant TO PUBLIC;
+GRANT SELECT ON Post TO PUBLIC;
+
+-- Add sample records
+INSERT INTO Student VALUES ('bw12', 'Bruce', 'Wayne', 'I am vengance');
+INSERT INTO Student VALUES ('ck23', 'Clark', 'Kent', 'Up, up, and away!');
+INSERT INTO Student VALUES ('dp34', 'Diana', 'Prince', 'Fighting doesn''t make you a hero');
+
+INSERT INTO Restaurant VALUES ('Johnny''s', '3201 Burton St SE, Grand Rapids, MI', '09:00', '21:00', 1, .20);
+INSERT INTO Restaurant VALUES ('Peet''s', '3201 Burton St SE, Grand Rapids, MI', '08:00', '20:00', 1, .15);
+INSERT INTO Restaurant VALUES ('Ucello''s Ristorante', '2630 E Beltline Ave SE, Grand Rapids, MI 49546', '11:00', '02:00', 2, .10);
+INSERT INTO Restaurant VALUES ('Panera Bread', '5630 28th Street SE, Grand Rapids, MI 49546', '08:00', '18:00', 3, .20);
+
+INSERT INTO Post VALUES ('bw12', 'Johnny''s', '3201 Burton St SE, Grand Rapids, MI', '2021-10-31 10:23:54', '2021-11-02 10:30');
+INSERT INTO Post VALUES ('ck23', 'Panera Bread', '5630 28th Street SE, Grand Rapids, MI 49546', '2021-10-28 10:30', '2021-10-31 12:25');
+
+-- ADD sample queries
+
+SELECT * FROM Student
+
+SELECT * FROM Restaurant
+
+SELECT Student.firstName, Student.lastName, restaurantName, restaurantAddress, meetupTime, postTime, priceRange
+FROM Post, Restaurant, Student  
+WHERE studentEmail = Student.email AND
+Restaurant.name = restaurantName AND
+Post.studentEmail = 'ck23'
