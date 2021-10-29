909bf0594370fae9205b19f34af1efa8c8d9789c
as76 <as76@maroon33.cs.calvin.edu>
2021-10-28 21:53:39 -0400

    changed datatype of discounts to strings

--- sql/KnightBites.sql
+++ sql/KnightBites.sql
@@ -17,7 +17,7 @@ CREATE TABLE Restaurant (
 	openingTime time,
 	closingTime time,
 	priceRange integer, -- Three tiers of prices ranging from 1 through 3
-	discount float,
+	discount varchar(60),
 	PRIMARY KEY(name, address)
 	);
 
@@ -41,22 +41,22 @@ INSERT INTO Student VALUES ('bw12', 'Bruce', 'Wayne', 'I am vengance');
 INSERT INTO Student VALUES ('ck23', 'Clark', 'Kent', 'Up, up, and away!');
 INSERT INTO Student VALUES ('dp34', 'Diana', 'Prince', 'Fighting doesn''t make you a hero');
 
-INSERT INTO Restaurant VALUES ('Johnny''s', '3201 Burton St SE, Grand Rapids, MI', '09:00', '21:00', 1, .20);
-INSERT INTO Restaurant VALUES ('Peet''s', '3201 Burton St SE, Grand Rapids, MI', '08:00', '20:00', 1, .15);
-INSERT INTO Restaurant VALUES ('Ucello''s Ristorante', '2630 E Beltline Ave SE, Grand Rapids, MI 49546', '11:00', '02:00', 2, .10);
-INSERT INTO Restaurant VALUES ('Panera Bread', '5630 28th Street SE, Grand Rapids, MI 49546', '08:00', '18:00', 3, .20);
+INSERT INTO Restaurant VALUES ('Johnny''s', '3201 Burton St SE, Grand Rapids, MI', '09:00', '21:00', 1, '10% off');
+INSERT INTO Restaurant VALUES ('Peet''s', '3201 Burton St SE, Grand Rapids, MI', '08:00', '20:00', 1, '15% off');
+INSERT INTO Restaurant VALUES ('Ucello''s Ristorante', '2630 E Beltline Ave SE, Grand Rapids, MI 49546', '11:00', '02:00', 2, '$2.00 off');
+INSERT INTO Restaurant VALUES ('Panera Bread', '5630 28th Street SE, Grand Rapids, MI 49546', '08:00', '18:00', 3, '$5 off on $20 or more');
 
 INSERT INTO Post VALUES ('bw12', 'Johnny''s', '3201 Burton St SE, Grand Rapids, MI', '2021-10-31 10:23:54', '2021-11-02 10:30');
 INSERT INTO Post VALUES ('ck23', 'Panera Bread', '5630 28th Street SE, Grand Rapids, MI 49546', '2021-10-28 10:30', '2021-10-31 12:25');
 
 -- ADD sample queries
 
-SELECT * FROM Student
+-- SELECT * FROM Student
 
-SELECT * FROM Restaurant
+-- SELECT discount FROM Restaurant
 
-SELECT Student.firstName, Student.lastName, restaurantName, restaurantAddress, meetupTime, postTime, priceRange
-FROM Post, Restaurant, Student  
-WHERE studentEmail = Student.email AND
-Restaurant.name = restaurantName AND
-Post.studentEmail = 'ck23'
+-- SELECT Student.firstName, Student.lastName, restaurantName, restaurantAddress, meetupTime, postTime, priceRange
+-- FROM Post, Restaurant, Student  
+-- WHERE studentEmail = Student.email AND
+-- Restaurant.name = restaurantName AND
+-- Post.studentEmail = 'ck23'
