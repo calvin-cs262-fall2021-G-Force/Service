721bce15ad0c2bd80bfffbf2e2d36d0fe77e1750
iko2 <iko2@remotel2.cs.calvin.edu>
2021-11-21 19:09:29 -0500

    added restaurants

--- sql/knightBites.sql
+++ sql/knightBites.sql
@@ -38,14 +38,29 @@ GRANT SELECT ON Restaurant TO PUBLIC;
 GRANT SELECT ON Post TO PUBLIC;
 
 -- Add sample records
-INSERT INTO Student VALUES ('bw12', 'Bruce', 'Wayne', 'I am vengance');
+INSERT INTO Student VALUES ('bw12', 'Bruce', 'Wayne', 'I am vengance'); 
 INSERT INTO Student VALUES ('ck23', 'Clark', 'Kent', 'Up, up, and away!');
 INSERT INTO Student VALUES ('dp34', 'Diana', 'Prince', 'Fighting doesn''t make you a hero');
 
-INSERT INTO Restaurant VALUES (1,'Johnny''s', '3201 Burton St SE, Grand Rapids, MI', '09:00', '21:00', 1, '20% off');
-INSERT INTO Restaurant VALUES (2, 'Peet''s', '3201 Burton St SE, Grand Rapids, MI', '08:00', '20:00', 1, '$15 off till 2pm');
-INSERT INTO Restaurant VALUES (3, 'Ucello''s Ristorante', '2630 E Beltline Ave SE, Grand Rapids, MI 49546', '11:00', '02:00', 2, '10% off');
-INSERT INTO Restaurant VALUES (4, 'Panera Bread', '5630 28th Street SE, Grand Rapids, MI 49546', '08:00', '18:00', 3, '$2 off');
+INSERT INTO Restaurant VALUES (1,'Uccelo''s Ristorante', '2630 E Beltline Ave SE, Grand Rapids, MI 49546', '11:00', '23:00', 1, '15% off');
+INSERT INTO Restaurant VALUES (2, 'Anna''s House', '2409 E Beltline Ave SE, Grand Rapids, MI 49546', '06:00', '15:00', 1, '10% off till 2pm');
+INSERT INTO Restaurant VALUES (3, 'IHOP', '5039, 28th Street SE, MI 49546', '6:00', '22:00', 2, '20% off');
+INSERT INTO Restaurant VALUES (4, 'Papa Johns', '4236 Kalamazoo Ave SE, Grand Rapids, MI 49508', '08:00', '23:00', 3, 'Large 1 Toppings go for $6.99 and Large 3 Toppings go for $10');
+INSERT INTO Restaurant VALUES (5, 'Eastern Floral', '2836 Broadmoor Ave SE, Grand Rapids, MI 49512', '09:00', '17:30', 3, '10% off');
+INSERT INTO Restaurant VALUES (6, 'Schuil Coffee', '3679 29th St SE, Grand Rapids, MI 49512', '07:00', '18:00', 3, '20% off');
+INSERT INTO Restaurant VALUES (7, 'Culver''s', '2510 E Beltline Ave SE, Grand Rapids, MI 49546', '10:00', '23:00', 1, '5% off');
+INSERT INTO Restaurant VALUES (8, 'Align Nutrition', '1144 E Paris Ave SE Suite 6, Grand Rapids, MI 49546', '08:00', '18:00', 3, '$1 off');
+INSERT INTO Restaurant VALUES (9, 'Applebees''s', '4955 28th St SE, Grand Rapids, MI 49546', '11:00', '24:00', 2, '10% off');
+INSERT INTO Restaurant VALUES (10, 'Panera Bread', '3770 28th St SE, Kentwood, MI 49512', '06:00', '19:00', 3, 'Limitied Coffee $8.99 a month');
+INSERT INTO Restaurant VALUES (11, 'Malamiah Juice Bar', '122 Oakes St SW #110, Grand Rapids, MI 49503', '07:00', '16:00', 1, '10% off');
+INSERT INTO Restaurant VALUES (12, 'Hall Street Bakery', '1200 Hall St SE, Grand Rapids, MI 49506', '6:30', '21:00', 2, '10% off');
+INSERT INTO Restaurant VALUES (13, 'Cafe Boba', '4314 Division Ave S, Kentwood, MI 49548', '14:00', '20:00', 1, '10% off');
+INSERT INTO Restaurant VALUES (14, 'Old Chicago', '3333 28th St SE Ste 1, Grand Rapids, MI 49512', '11:00', '22:00', 2, '$5 off on an order of $20 or more');
+INSERT INTO Restaurant VALUES (15, 'Tallarico''s Boardwalk Subs', '3083 Broadmoor Ave SE, Kentwood, MI 49512', '10:30', '15:00', 1, '15% off');
+INSERT INTO Restaurant VALUES (16, 'Panda Express', '3170 28th St SE, Kentwood, MI 49508', '10:30', '21:00', 1, '20% off');
+INSERT INTO Restaurant VALUES (17, 'Bitter End Coffehouse', '752 Fulton St W, Grand Rapids, MI 49504', '01:00', '24:00', 1, '10% off');
+
+
 
 INSERT INTO Post VALUES ('bw12', 1 , 'Wanna have lunch at Johnny''s?','I am going to Johnny''s with my friends in a bit. You can join if you want to','2021-10-31 10:23:54', '2021-11-02 10:30');
 INSERT INTO Post VALUES ('ck23', 2 , 'NEED COFFEE','I need to get coffee NOW! Anybody wanna join me?','2021-10-28 10:30', '2021-10-31 12:25');
