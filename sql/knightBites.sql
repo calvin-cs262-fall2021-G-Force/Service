b5e037eb19b5d1196ff93be5848af8ca62abb133
anjanasainju <50439374+anjanasainju@users.noreply.github.com>
2021-12-03 17:48:53 -0500

    Revert "changed postid type to serial"

--- sql/knightBites.sql
+++ sql/knightBites.sql
@@ -38,7 +38,7 @@ CREATE TABLE Post (
 );
 
 CREATE TABLE EventAttendee (
-	postID SERIAL,
+	postID int,
 	FOREIGN KEY (postID) REFERENCES Post(ID),
 	studentEmail varchar(30),
 	FOREIGN KEY (studentEmail) REFERENCES Student(email)
