71963bb46fd7d572d9d08980a8698feecf865b42
anjanasainju <anjanasainju@gmail.com>
2021-12-10 20:15:50 -0500

    modified postdetails query to change the order by

--- knightBitesService.js
+++ knightBitesService.js
@@ -88,7 +88,7 @@ function readPosts(req, res, next) {
 
 function readPostsDetails(req, res, next) {
   db.manyOrNone(
-    "SELECT * FROM Post, Student, Restaurant WHERE Post.studentEmail = Student.email AND Post.restaurantId = Restaurant.restaurantID ORDER BY postTime DESC"
+    "SELECT * FROM Post, Student, Restaurant WHERE Post.studentEmail = Student.email AND Post.restaurantId = Restaurant.restaurantID ORDER BY ${sort}} DESC",req.params
   )
     .then((data) => {
       res.send(data);
