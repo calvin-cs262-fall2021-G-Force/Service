546c86b5e2ea4ad7ad510e2c19aac8d6469f6e80
Aayam <aayam2071@gmail.com>
2021-12-13 23:02:27 -0500

    added queries

--- sql/knightBites-queries.sql
+++ sql/knightBites-queries.sql
@@ -6,9 +6,16 @@ SELECT * FROM Student
 -- Get all records from the Restaurant table
 SELECT * FROM Restaurant
 
--- Get name of the student, restaurant information, and meetup information
-SELECT Student.firstName, Student.lastName, restaurantName, restaurantAddress, meetupTime, postTime, priceRange
-FROM Post, Restaurant, Student  
-WHERE studentEmail = Student.email AND
-Restaurant.name = restaurantName AND
-Post.studentEmail = 'ck23'
+-- Get all the information about each post, student who made the post, and restaurant 
+-- the post is associated with from the database in descending order of post time
+SELECT *
+FROM Post, Student, Restaurant
+WHERE Post.studentEmail = Student.email
+AND Post.restaurantId = Restaurant.restaurantID
+ORDER BY posttime DESC
+
+-- Get the email, firstname, and lastname of all students who are attending an event.
+SELECT studentEmail, firstname, lastname
+FROM EventAttendee, Student
+WHERE EventAttendee.studentEmail=Student.email
+AND postid = 83
